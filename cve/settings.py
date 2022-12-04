5ec8a74dfef9938f8d65f796d7caf35c3b940272
Hakccer <81087743+Hakccer@users.noreply.github.com>
2022-12-04 14:21:32 +0530

    Update settings.py

--- cve/settings.py
+++ cve/settings.py
@@ -29,8 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['cve-production.up.railway.app']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'https://symphonious-sunflower-173e5c.netlify.app'
 ]
 
 # Application definition
